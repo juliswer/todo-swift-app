4ff4f2d986e842077620ca01d512c47d6e532540
Julian Swerdlin <julianswer36@gmail.com>
2023-09-02 15:14:46 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Julian Swerdlin on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
